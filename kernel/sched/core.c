b5bafdf46601a2d68723dc86781db4bb42291d70
Hamad Al Marri <hamad.s.almarri@gmail.com>
2021-01-07 17:41:56 +0300

    Cachy-5.7-r9

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3036,6 +3036,11 @@ void wake_up_new_task(struct task_struct *p)
 	update_rq_clock(rq);
 	post_init_entity_util_avg(p);
 
+#ifdef CONFIG_CACHY_SCHED
+	if (!cachy_harsh_mode)
+		p->se.hrrn_start_time = sched_clock();
+#endif
+
 	activate_task(rq, p, ENQUEUE_NOCLOCK);
 	trace_sched_wakeup_new(p);
 	check_preempt_curr(rq, p, WF_FORK);
@@ -6662,6 +6667,10 @@ void __init sched_init(void)
 {
 	unsigned long ptr = 0;
 	int i;
+
+#ifdef CONFIG_CACHY_SCHED
+	printk(KERN_INFO "Cachy CPU scheduler v5.9-r9 by Hamad Al Marri.");
+#endif
 
 	wait_bit_init();
 
