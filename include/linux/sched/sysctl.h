02befde445c400f6db2e9943684c4bb405024743
Hamad Al Marri <hamad@cachyos.org>
2021-07-12 20:50:42 +0300

    yield_rework

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -36,6 +36,7 @@ extern unsigned int sysctl_sched_wakeup_granularity;
 extern unsigned int interactivity_factor;
 extern unsigned int interactivity_threshold;
 extern unsigned int cacule_max_lifetime;
+extern int sched_yield_type;
 #endif
 
 enum sched_tunable_scaling {
