a822aa5c200a05e21e46966fa80f5ceb634e068d
hamadmarri <hamad.s.almarri@gmail.com>
2021-07-28 06:10:22 +0300

    Multi factors scores

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -34,6 +34,10 @@ extern unsigned int sysctl_sched_child_runs_first;
 #ifdef CONFIG_CACULE_SCHED
 extern unsigned int interactivity_factor;
 extern unsigned int cacule_max_lifetime;
+extern unsigned int cache_factor;
+extern unsigned int cache_divisor;
+extern unsigned int starve_factor;
+extern unsigned int starve_divisor;
 extern int cacule_yield;
 #endif
 
