2d8b3bda7f0a16c1f2d73b2c87ccb00fc50b8989
hamadmarri <hamad.s.almarri@gmail.com>
2021-08-01 00:26:20 +0300

    move yield unmark to pick_next_task_fair after pick_next_entity

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4914,10 +4914,6 @@ static void put_prev_entity(struct cfs_rq *cfs_rq, struct sched_entity *prev)
 		update_load_avg(cfs_rq, prev, 0);
 	}
 
-#ifdef CONFIG_CACULE_SCHED
-	prev->cacule_node.vruntime &= YIELD_UNMARK;
-#endif
-
 	cfs_rq->curr = NULL;
 }
 
@@ -7594,6 +7590,11 @@ simple:
 		set_next_entity(cfs_rq, se);
 		cfs_rq = group_cfs_rq(se);
 	} while (cfs_rq);
+
+#ifdef CONFIG_CACULE_SCHED
+	if (prev)
+		prev->se.cacule_node.vruntime &= YIELD_UNMARK;
+#endif
 
 	p = task_of(se);
 
